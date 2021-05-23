edfbde1f953f534519f9ebdee2f48b676fa1eb15
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-05-23 15:50:33 +0530

    Understanding Hoisting

--- JavaScript - Understanding the Weird Parts/02-executation-context-hoisting/app.js
+++ JavaScript - Understanding the Weird Parts/02-executation-context-hoisting/app.js
@@ -5,10 +5,18 @@
     And at the global level those two things are equal so window object = this
 */
 
+// using variable and function 'before' their declation
+b();
+console.log(a);
+
 // global variable. can be accessed as 'a' or window.a
-var a = 'Hello World';
+var a = 'Hello World!';
 
 // global function. can be accessed as 'b' or window.b
 function b() {
-    
+    console.log('Called b!');
 }
+
+// using variable and function 'after' their declation
+b();
+console.log(a);
