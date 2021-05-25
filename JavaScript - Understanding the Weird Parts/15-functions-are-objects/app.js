2d5c8832186042f2474acfa69c642145415ccee7
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-05-25 19:00:25 +0530

    Functions are Objects

--- JavaScript - Understanding the Weird Parts/15-functions-are-objects/app.js
+++ JavaScript - Understanding the Weird Parts/15-functions-are-objects/app.js
@@ -9,3 +9,6 @@ greet.language = 'english';
 console.log(greet.language); // => english
 console.log(greet);          // this prints function 
 console.log(greet.name);     // this prints the function name - greet
+
+// IMP
+// in browser console, type "greet.", you will see all available variables and methods for this function.
