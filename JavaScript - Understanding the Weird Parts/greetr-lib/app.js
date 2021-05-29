e04207e36c385b953463f627191e9cadef8641a2
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-05-29 16:33:05 +0530

    Using Our Greetr library

--- JavaScript - Understanding the Weird Parts/greetr-lib/app.js
+++ JavaScript - Understanding the Weird Parts/greetr-lib/app.js
@@ -1,13 +1,29 @@
+// gets a new object (the architecture allows us to not have to use the 'new' keyword here)
 var g = G$('Sameer', 'Bhilare');
 
 //g.greet();
 //g.greet().greet(true);
-
 //g.greet().setLang('es').greet(true);
+//g.greet().setLang('fr').greet(true); // not supported lang
+//g.greet().setLang('es').greet(true).log();
+//g.HTMLGreeting('#greeting'); // jquery
 
-//g.greet().setLang('fr').greet(true);
 
+
+// use our chainable methods
 //g.greet().setLang('es').greet(true).log();
 
-// jquery
-g.HTMLGreeting('#greeting');
+
+// let's use our object on the click of the login button
+$('#login').click(function() {
+   
+    // create a new 'Greetr' object (let's pretend we know the name from the login)
+    var loginGrtr = G$('Sameer', 'Bhilare');
+    
+     // hide the login on the screen
+    $('#logindiv').hide();
+    
+     // fire off an HTML greeting, passing the '#greeting' as the selector and the chosen language, and log the welcome as well
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+    
+});
